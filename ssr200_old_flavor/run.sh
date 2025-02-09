bf71e12ed0e0d61e9582418a682cb81f0d722c68
エッケザックス <22173829+kagekiyo7@users.noreply.github.com>
2025-02-09 22:19:30 +0900

    Update run.sh

--- ssr200_old_flavor/run.sh
+++ ssr200_old_flavor/run.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 set -e
 
+if [ -z "$1" ]; then
+    echo "Error: No input file provided."
+    exit 1
+fi
+
 sh_dir="$(dirname "$(readlink -f "$0")")"
 working_dir="$(dirname "$(readlink -f "$1")")"
 filename=$(basename "$1" | sed 's/\.[^.]*$//')
-outputdir="${filename}_outout"
+outputdir="${filename}_fats"
 
-cd "$working_dir"
+cd "$working_dir" || { echo "Error: Failed to change directory to $working_dir"; exit 1; }
 echo "Started remapping the NAND."
 python3 "$sh_dir/convert_old_ssr200.py" "$1" "${filename}.oob" "${filename}_remapped.bin"
 echo "Success!: ${filename}_remapped.bin"
